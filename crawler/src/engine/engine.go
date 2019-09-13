bb6bd26fd4f10b59113f677f47427241a0575565
bc l <774531697@qq.com>
2019-09-13 21:18:54 +0800

    nil

--- crawler/src/engine/engine.go
+++ crawler/src/engine/engine.go
@@ -5,20 +5,20 @@ import (
 	"log"
 )
 
-func Run(seeds ...Request){
+func Run(seeds ...Request) {
 	var requests []Request
 	for _, r := range seeds {
 		requests = append(requests, r)
 	}
 
-	for len(requests)  > 0 {
+	for len(requests) > 0 {
 		r := requests[0]
 		requests = requests[1:]
 
 		log.Printf("Fetching %s", r.Url)
 		body, err := fetcher.Fetch(r.Url)
 		if err != nil {
-			log.Printf("Fetcher: error " + "fetching url %s: %v",
+			log.Printf("Fetcher: error "+"fetching url %s: %v",
 				r.Url, err)
 			continue
 		}
@@ -30,9 +30,6 @@ func Run(seeds ...Request){
 		for _, item := range parseResult.Items {
 			log.Printf("Got item %s", item)
 		}
-
-
-
 	}
 
 }
