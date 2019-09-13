01f252ab02532beafc735a4a71dfc320fee59f57
bc l <774531697@qq.com>
2019-09-13 21:12:22 +0800

    crawler

---
+++ crawler/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"engine"
+	"zhenai/parser"
+)
+
+func main() {
+	engine.Run(engine.Request{
+		Url:       "http://www.zhenai.com/zhenghun",
+		ParseFunc: parser.ParseCityList,
+	})
+
+
+}
+
+
